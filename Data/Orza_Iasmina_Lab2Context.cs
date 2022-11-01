6507f0211842dd23b205c9869af6e1f7993540f5
iasmina17 <iasmina_orza@yahoo.com>
2022-11-01 14:48:01 +0200

    Add project files.

---
+++ Data/Orza_Iasmina_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Orza_Iasmina_Lab2.Models;
+
+namespace Orza_Iasmina_Lab2.Data
+{
+    public class Orza_Iasmina_Lab2Context : DbContext
+    {
+        public Orza_Iasmina_Lab2Context (DbContextOptions<Orza_Iasmina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Orza_Iasmina_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Orza_Iasmina_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
